370888e9bc5ca1ecf032e6631b20d7bffce93b32
chenle02 <chenle02@gmail.com>
2023-11-26 00:29:56 -0500

    Add topics

---
+++ myzsh.zsh
@@ -0,0 +1,9 @@
+
+alias description="gh repo edit \
+  --description 'Financial Mathematics: Open Slides' \
+  --homepage 'https://doi.org/10.5281/zenodo.10207028' \
+  --add-topic 'teaching' \
+  --add-topic 'slides' \
+  --add-topic 'finacial-math' \
+  --add-topic 'latex' \
+  "
